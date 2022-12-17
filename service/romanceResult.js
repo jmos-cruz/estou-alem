681b5bc65cbc94c341283f29c9e7f23b0d009caf
Joao Cruz <jmos.cruz@gmail.com>
2022-12-17 11:59:48 +0000

    Created remote repo for website hosting

---
+++ service/romanceResult.js
@@ -0,0 +1,128 @@
+const maleHetSer = [{link1:'https://goo.gl/maps/6ZsrzWKfXEHSFhKu9'}, {link2: 'https://tinder.com/pt-PT'}]; //Male + Hetero + Serious
+const maleHomSer = [{link1:'https://www.grindr.com/'}, {link2: 'https://tinder.com/pt-PT'}]; //Male + Homo + Serious
+const maleBiSer = [{link1:'https://goo.gl/maps/6ZsrzWKfXEHSFhKu9'}, {link2:'https://tinder.com/pt-PT'}, {link3:'https://www.grindr.com/'}]; //Male + Bi + Serious
+const maleHetOne = [{link1:'https://goo.gl/maps/mgLUzE84QDWPcdS76'}, {link2:'https://goo.gl/maps/iZR6B1eXs9FLBvzN8'}, {link3:'https://www.classificadosx.net/pt/lista/1-mulheres'}]; //Male + Hetero + One night stand
+const maleHomOne = [{link1:'https://www.classificadosx.net/pt/lista/112-gays'}, {link2:'https://goo.gl/maps/TXNzYuMV8JevVcK49'}, {link3:'https://goo.gl/maps/2RAFPB3bjXE5nunY7'}]; //Male + Homo + One night stand
+const maleBiOne = [{link1:'https://goo.gl/maps/mgLUzE84QDWPcdS76'}, {link2:'https://goo.gl/maps/iZR6B1eXs9FLBvzN8'}, {link3:'https://www.classificadosx.net/pt/lista/1-mulheres'}, {link4:'https://www.classificadosx.net/pt/lista/112-gays'}, {link5:'https://goo.gl/maps/TXNzYuMV8JevVcK49'}]; //Male + Bi + One night stand
+const maleHetFri = [{link1:'https://goo.gl/maps/6ZsrzWKfXEHSFhKu9'}, {link2: 'https://www.ashleymadison.com/pt-pt/?gclid=Cj0KCQiAqOucBhDrARIsAPCQL1al0GGufwn0N091hW2HyuaZzEHHah_6klXbNO6_S-rdDHHpy6vBF7IaAq4iEALw_wcB&utm_logged=1&utm_term=%2Bashley%20%2Bmadison&utm_campaign=Ashley%2BMadison%2B-%2BPT%2B-%2BPortuguese%2B-%2BBrand&am_utm=249512343&utm_source=google&utm_medium=cpc&adgroup=Brand%2B-%2BPT&utm_content=b'}]; //Male + Hetero + FwB
+const maleHomFri = [{link1:'https://www.grindr.com/'}, {link2:'https://tinder.com/pt-PT'}]; //Male + Homo + FwB
+const maleBiFri = [{link1:'https://goo.gl/maps/6ZsrzWKfXEHSFhKu9'}, {link2:'https://www.ashleymadison.com/pt-pt/?gclid=Cj0KCQiAqOucBhDrARIsAPCQL1al0GGufwn0N091hW2HyuaZzEHHah_6klXbNO6_S-rdDHHpy6vBF7IaAq4iEALw_wcB&utm_logged=1&utm_term=%2Bashley%20%2Bmadison&utm_campaign=Ashley%2BMadison%2B-%2BPT%2B-%2BPortuguese%2B-%2BBrand&am_utm=249512343&utm_source=google&utm_medium=cpc&adgroup=Brand%2B-%2BPT&utm_content=b'}, {link3:'https://www.grindr.com/'}, {link4:'https://tinder.com/pt-PT'}]; //Male + Bi + FwB
+const femHetSer = [{link1:'https://tinder.com/pt-PT'}, {link2:'https://goo.gl/maps/6ZsrzWKfXEHSFhKu9'}]; //Female + Hetero + Serious
+const femHomSer = [{link1:'https://tinder.com/pt-PT'}, {link2:'https://weareher.com/download/?_branch_match_id=776436355502982660'}]; //Female + Homo + Serious
+const femBiSer = [{link1:'https://tinder.com/pt-PT'}, {link2:'https://goo.gl/maps/6ZsrzWKfXEHSFhKu9'}, {link3:'https://tinder.com/pt-PT'}, {link4:'https://weareher.com/download/?_branch_match_id=776436355502982660'}]; //Female + Bi + Serious
+const femHetOne = [{link1:'https://goo.gl/maps/mgLUzE84QDWPcdS76'}, {link2:'https://www.classificadosx.net/pt/lista/2-homens'}]; //Female + Hetero + One night stand
+const femHomOne = [{link1:'https://www.instagram.com/planetamanas/'}, {link2:'https://www.classificadosx.net/pt/lista/133-lesbicas'}]; //Female + Homo + One night stand
+const femBiOne = [{link1:'https://goo.gl/maps/mgLUzE84QDWPcdS76'}, {link2:'https://www.classificadosx.net/pt/lista/2-homens'}, {link3:'https://www.instagram.com/planetamanas/'}, {link4:'https://www.classificadosx.net/pt/lista/133-lesbicas'}]; //Female + Bi + One night stand
+const femHetFri = [{link1:'https://www.ashleymadison.com/pt-pt/?gclid=Cj0KCQiAqOucBhDrARIsAPCQL1al0GGufwn0N091hW2HyuaZzEHHah_6klXbNO6_S-rdDHHpy6vBF7IaAq4iEALw_wcB&utm_logged=1&utm_term=%2Bashley%20%2Bmadison&utm_campaign=Ashley%2BMadison%2B-%2BPT%2B-%2BPortuguese%2B-%2BBrand&am_utm=249512343&utm_source=google&utm_medium=cpc&adgroup=Brand%2B-%2BPT&utm_content=b'}, {link2:'https://goo.gl/maps/6ZsrzWKfXEHSFhKu9'}]; //Female + Hetero + FwB
+const femHomFri = [{link1:'https://tinder.com/pt-PT'}, {link2:'https://weareher.com/download/?_branch_match_id=776436355502982660'}]; //Female + Homo + FwB
+const femBiFri = [{link1:'https://www.ashleymadison.com/pt-pt/?gclid=Cj0KCQiAqOucBhDrARIsAPCQL1al0GGufwn0N091hW2HyuaZzEHHah_6klXbNO6_S-rdDHHpy6vBF7IaAq4iEALw_wcB&utm_logged=1&utm_term=%2Bashley%20%2Bmadison&utm_campaign=Ashley%2BMadison%2B-%2BPT%2B-%2BPortuguese%2B-%2BBrand&am_utm=249512343&utm_source=google&utm_medium=cpc&adgroup=Brand%2B-%2BPT&utm_content=b'}, {link2:'https://goo.gl/maps/6ZsrzWKfXEHSFhKu9'}, {link3:'https://tinder.com/pt-PT'}, {link4:'https://weareher.com/download/?_branch_match_id=776436355502982660'}]; //Female + Bi + FwB
+
+
+const sexQuestions = [
+    {
+      arr: ['a', 'a', 'a'],
+        destination: maleHetSer[Math.floor(Math.random() * maleHetSer.length)]
+        },
+  
+    {
+      arr: ['a', 'a', 'b'],
+        destination: maleHetOne[Math.floor(Math.random() * maleHetOne.length)]
+    },
+  
+    {
+      arr: ['a', 'a', 'c'],
+        destination: maleHetFri[Math.floor(Math.random() * maleHetFri.length)],
+    },
+
+    {
+        arr: ['a', 'a', 'd'],
+        destination: maleHetFri[Math.floor(Math.random() * maleBiFri.length)],
+    },
+  
+    {
+      arr: ['a', 'b', 'a'],
+        destination: maleHomSer[Math.floor(Math.random() * maleHomSer.length)],
+    },
+  
+    {
+      arr: ['a', 'b', 'b'],
+        destination: maleHomOne[Math.floor(Math.random() * maleHomOne.length)],
+    },
+  
+    {
+      arr: ['a', 'b', 'c'],
+        destination: maleHomFri[Math.floor(Math.random() * maleHomFri.length)],
+    },
+  
+    {
+      arr: ['a', 'c', 'a'],
+        destination: maleBiSer[Math.floor(Math.random() * maleBiSer.length)],
+    },
+  
+    {
+      arr: ['a', 'c', 'b'],
+        destination: maleBiOne[Math.floor(Math.random() * maleBiOne.length)],
+    },
+  
+    {
+      arr: ['a', 'c', 'c'],
+        destination: maleBiFri[Math.floor(Math.random() * maleBiFri.length)],
+    },
+  
+    {
+      arr: ['b', 'a', 'a'],
+        destination: femHetSer[Math.floor(Math.random() * femHetSer.length)],
+    },
+  
+    {
+      arr: ['b', 'a', 'b'],
+        destination: femHetOne[Math.floor(Math.random() * femHetOne.length)],
+    },
+  
+    {
+      arr: ['b', 'a', 'c'],
+        destination: femHetFri[Math.floor(Math.random() * femHetFri.length)],
+    },
+  
+    {
+      arr: ['b', 'b', 'a'],
+        destination: femHomSer[Math.floor(Math.random() * femHomSer.length)],
+    },
+  
+    {
+      arr: ['b', 'b', 'b'],
+        destination: femHomOne[Math.floor(Math.random() * femHomOne.length)],
+    },
+  
+    {
+      arr: ['b', 'b', 'c'],
+        destination: femHomFri[Math.floor(Math.random() * femHomFri.length)],
+    },
+  
+    {
+      arr: ['b', 'c', 'a'],
+        destination: femBiSer[Math.floor(Math.random() * femBiSer.length)],
+    },
+  
+    {
+      arr: ['b', 'c', 'b'],
+        destination: femBiOne[Math.floor(Math.random() * femBiOne.length)],
+    },
+  
+    {
+      arr: ['b', 'c', 'c'],
+        destination: femBiFri[Math.floor(Math.random() * femBiFri.length)],
+    },
+  ];
+  
+  let filterResult = (questArr) => {
+    console.log(questArr);
+    for (let i = 0; i < sexQuestions.length; i++) {
+      if (sexQuestions[i].arr.toString() === questArr.toString()) {
+        console.log(sexQuestions[i]);
+        return {link: Object.values(sexQuestions[i].destination)[0], image:"/Img/sexual.png"};
+      }
+    }
+  };
+
+  export { filterResult };
